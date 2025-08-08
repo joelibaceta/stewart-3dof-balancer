b4bd72fe35399cedcf9f1eb8eb0196ddacb09ffb
Joel Ibaceta <mail@joelibaceta.com>
2025-08-08 04:16:49 -0500

    plate as constraints

--- simulation.py
+++ simulation.py
@@ -17,6 +17,19 @@ p.setGravity(0, 0, -9.81)
 # Cargar tu modelo URDF (ajusta el nombre si es distinto)
 robot_id = p.loadURDF("robot.urdf", [0, 0, 0.1], useFixedBase=True)
 
+top_col = p.createCollisionShape(p.GEOM_MESH, fileName="meshes/triangle_filled.stl", meshScale=[0.001]*3)
+top_vis = p.createVisualShape(p.GEOM_MESH, fileName="meshes/triangle_filled.stl", meshScale=[0.001]*3, rgbaColor=[1, 0, 0, 1])
+
+top_plate_id = p.createMultiBody(
+    baseMass=0.1,
+    baseCollisionShapeIndex=top_col,
+    baseVisualShapeIndex=top_vis,
+    basePosition=[0, 0, 0.18]
+)
+
+for i in range(p.getNumJoints(robot_id)):
+    print(i, p.getJointInfo(robot_id, i)[1].decode('utf-8'))
+
 # Cámara (opcional)
 p.resetDebugVisualizerCamera(
     cameraDistance=0.5,
